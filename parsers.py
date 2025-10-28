f297cf3630a1b91f85f5e225beade1b89431a05c
ajdavis25 <98492932+ajdavis25@users.noreply.github.com>
2025-10-28 10:43:17 -0500

    added emails and missing requirements

--- parsers.py
+++ parsers.py
@@ -1,9 +1,7 @@
 import io, re, urllib.request
 from pdfminer.high_level import extract_text
 
-
 HEADERS = [r"conclusion[s]?", r"summary", r"concluding remarks", r"discussion"]
-
 
 def try_extract_conclusion(pdf_url, char_limit=1200):
     try:
